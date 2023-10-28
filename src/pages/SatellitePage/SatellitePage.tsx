02bca650fc00e1b05ba91f35a93148cc82f7e9a4
tkoibaev <tam.koib12@gmail.com>
2023-10-28 16:22:28 +0300

    add mapping

--- src/pages/SatellitePage/SatellitePage.tsx
+++ src/pages/SatellitePage/SatellitePage.tsx
@@ -22,6 +22,11 @@ import useLocalStorage from "use-local-storage";
 import userSvg from "assets/icons/sun.png";
 import rootStore from "store/RootStore";
 import MultiDropdown from "components/DropDown";
+import Map from "components/Map";
+
+import Canvas from "components/Canvas";
+
+const point = { x: 95, y: 95 };
 
 const SatellitePage = () => {
   return (
@@ -31,9 +36,7 @@ const SatellitePage = () => {
       })}
     >
       <Header></Header>
-      <div className={styles["main__page-wrapper"]}>
-      SatellitePage !!!
-      </div>
+      <div className={styles["main__page-wrapper"]}></div>
     </div>
   );
 };
